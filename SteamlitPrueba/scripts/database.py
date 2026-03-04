231bd8007178c631489e3b01765aedb0c16d3862
Yefersom10 <palitidoxd23@gmail.com>
2026-03-03 23:52:40 -0500

    Configuración PostgreSQL para producción

--- SteamlitPrueba/scripts/database.py
+++ SteamlitPrueba/scripts/database.py
@@ -1,12 +1,24 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-DATABASE_URL = "sqlite:///clima-pitacho.db"
+load_dotenv()
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Se usa SQLite par el modo local y PostgreSQL para producción
+if not DATABASE_URL:
+    DATABASE_URL = "sqlite:///clima-pitacho.db"
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(
+        DATABASE_URL,
+        pool_pre_ping=True
+    )
 
 SessionLocal = sessionmaker(
     autocommit=False,

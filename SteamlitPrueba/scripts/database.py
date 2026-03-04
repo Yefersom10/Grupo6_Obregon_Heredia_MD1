607d96b0cf97fce48abbd8154dfa3be839cfbdea
Yefersom10 <palitidoxd23@gmail.com>
2026-03-04 00:56:18 -0500

    Refactor database connection logic for Streamlit Cloud and local environments

--- SteamlitPrueba/scripts/database.py
+++ SteamlitPrueba/scripts/database.py
@@ -1,34 +1,34 @@
-import os
 import streamlit as st
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from urllib.parse import quote_plus
 
-# Detecta si está en Streamlit Cloud
+Base = declarative_base()
+
+# 🔥 Está en Streamlit Cloud
 if "DB_HOST" in st.secrets:
+
     DB_HOST = st.secrets["DB_HOST"]
     DB_PORT = st.secrets["DB_PORT"]
     DB_USER = st.secrets["DB_USER"]
-    DB_PASSWORD = st.secrets["DB_PASSWORD"]
+    DB_PASSWORD = quote_plus(st.secrets["DB_PASSWORD"])
     DB_NAME = st.secrets["DB_NAME"]
 
     DATABASE_URL = (
         f"postgresql://{DB_USER}:{DB_PASSWORD}"
         f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
-else:
-    DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Fallback local
-if not DATABASE_URL:
-    DATABASE_URL = "sqlite:///clima-pitacho.db"
-    engine = create_engine(
-        DATABASE_URL,
-        connect_args={"check_same_thread": False}
-    )
-else:
     engine = create_engine(
         DATABASE_URL,
         pool_pre_ping=True
+    )
+
+# 💻 Estás en local
+else:
+    engine = create_engine(
+        "sqlite:///clima-pitacho.db",
+        connect_args={"check_same_thread": False}
     )
 
 SessionLocal = sessionmaker(
@@ -36,5 +36,3 @@ SessionLocal = sessionmaker(
     autoflush=False,
     bind=engine
 )
-
-Base = declarative_base()

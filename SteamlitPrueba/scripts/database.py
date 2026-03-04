6a037bd3e02d077fd44db549b28f6ca88bab7cea
Yefersom10 <palitidoxd23@gmail.com>
2026-03-04 00:41:42 -0500

    Configurar conexión a la base de datos para Streamlit Cloud

--- SteamlitPrueba/scripts/database.py
+++ SteamlitPrueba/scripts/database.py
@@ -1,13 +1,24 @@
 import os
+import streamlit as st
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
 
-load_dotenv()
+# Detecta si está en Streamlit Cloud
+if "DB_HOST" in st.secrets:
+    DB_HOST = st.secrets["DB_HOST"]
+    DB_PORT = st.secrets["DB_PORT"]
+    DB_USER = st.secrets["DB_USER"]
+    DB_PASSWORD = st.secrets["DB_PASSWORD"]
+    DB_NAME = st.secrets["DB_NAME"]
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+    DATABASE_URL = (
+        f"postgresql://{DB_USER}:{DB_PASSWORD}"
+        f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
+else:
+    DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Se usa SQLite par el modo local y PostgreSQL para producción
+# Fallback local
 if not DATABASE_URL:
     DATABASE_URL = "sqlite:///clima-pitacho.db"
     engine = create_engine(
